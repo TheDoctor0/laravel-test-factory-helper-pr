74a5492f3f202c23b2ede6e7032fb3b01da69a6f
The Doctor <dawid.janik95@gmail.com>
2020-09-03 18:32:22 +0200

    Generate factories for test models.

---
+++ database/factories/TestSqliteFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(App\TestSqlite::class, function (Faker $faker) {
+    return [
+        'city' => $faker->city,
+        'company' => $faker->company,
+        'country' => $faker->country,
+        'description' => $faker->text,
+        'email' => $faker->safeEmail,
+        'first_name' => $faker->firstName,
+        'firstname' => $faker->firstName,
+        'guid' => $faker->uuid,
+        'last_name' => $faker->lastName,
+        'lastname' => $faker->lastName,
+        'lat' => $faker->latitude,
+        'latitude' => $faker->latitude,
+        'lng' => $faker->longitude,
+        'longitude' => $faker->longitude,
+        'name' => $faker->name,
+        'password' => bcrypt($faker->password),
+        'phone' => $faker->phoneNumber,
+        'phone_number' => $faker->phoneNumber,
+        'postcode' => $faker->postcode,
+        'postal_code' => $faker->postcode,
+        'remember_token' => Str::random(10),
+        'slug' => $faker->slug,
+        'street' => $faker->streetName,
+        'address1' => $faker->streetAddress,
+        'address2' => $faker->secondaryAddress,
+        'summary' => $faker->text,
+        'url' => $faker->url,
+        'user_name' => $faker->userName,
+        'username' => $faker->userName,
+        'uuid' => $faker->uuid,
+        'zip' => $faker->postcode,
+        'string' => $faker->word,
+        'text' => $faker->text,
+        'date' => $faker->date(),
+        'time' => $faker->time(),
+        'datetimetz' => $faker->dateTime(),
+        'datetime' => $faker->dateTime(),
+        'integer' => $faker->randomNumber(),
+        'bigint' => $faker->randomNumber(),
+        'smallint' => $faker->randomNumber(),
+        'decimal' => $faker->randomFloat(),
+        'float' => $faker->randomFloat(),
+        'boolean' => $faker->boolean,
+    ];
+});
